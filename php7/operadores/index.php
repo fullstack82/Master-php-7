101ed3d85b11e5a1bb1baa41e06ed265f8890e26
fullstack82 <fullstack8210@gmail.com>
2021-02-12 13:08:44 +0100

    Operadores

---
+++ php7/operadores/index.php
@@ -0,0 +1,44 @@
+<?php
+
+// Operadores Aritmeticos
+
+$numero1 = 4;
+$numero2 = 9;
+
+
+
+echo 'Suma:'.($numero1 + $numero2);
+echo '<br/>';
+echo 'Resta:'.($numero1 - $numero2);
+echo '<br/>';
+echo 'Multiplicación:'.($numero1 * $numero2);
+echo '<br/>';
+echo 'División:'.($numero1 / $numero2);
+echo '<br/>';
+echo 'Resto:'.($numero1 % $numero2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
