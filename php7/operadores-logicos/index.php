1dab76297adbdd852dd7f14559c0599f92956bc9
fullstack82 <fullstack8210@gmail.com>
2021-02-12 23:47:59 +0100

    operadores logicos

--- php7/operadores-logicos/index.php
+++ php7/operadores-logicos/index.php
@@ -1,3 +1,26 @@
-<?php 
-$numero = 35;
+<?php
+// Operadores logicos (&& = and, || = or , ! = no) los tres principales
 
+// Ejemplo numero 1.
+
+$edad1 = 18;
+$edad2 = 64;
+
+$edad_oficial = 55;
+
+if ($edad_oficial >= $edad1 && $edad_oficial <= $edad2) {
+    echo "Esta en edad de trabajar";
+} else {
+    echo "No, no esta en edad de trabajar";
+}
+
+echo "<hr/>";
+// Ejemplo numero 2. Usando el or (o) 
+
+$pais = "Mexico";
+
+if ($pais == "Mexico" || $pais == "España" || $pais == "Colombia") {
+    echo "En este pais se habla español";
+} else {
+    echo "En esta pais no se habla español";
+}
