48e705b5547eaf1a7f1faf2910d3db81878ff874
fullstack82 <fullstack8210@gmail.com>
2021-02-12 21:31:53 +0100

    condicionales if

--- php7/condicional-if/index.php
+++ php7/condicional-if/index.php
@@ -1,1 +1,18 @@
 <?php
+
+// Primer ejercicio
+$galletas = true;
+if ($galletas == true) {
+    echo 'Hay galletas';
+} else {
+    echo 'No hay galletas';
+};
+echo "<br/>";
+// Segundo ejercico
+$color = "azul";
+if($color == "rosa") {
+    echo 'El color es rosa';
+}else{
+    echo 'Es de otro color';
+};
+// Tercer ejercicio
