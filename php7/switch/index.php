f03df2adeaf4c842ea62564d3285cfe6d39e725c
fullstack82 <fullstack8210@gmail.com>
2021-02-12 23:54:42 +0100

    switch

--- php7/switch/index.php
+++ php7/switch/index.php
@@ -1,26 +1,24 @@
 <?php
-// Operadores logicos (&& = and, || = or , ! = no) los tres principales
+$dia = 4;
 
-// Ejemplo numero 1.
 
-$edad1 = 18;
-$edad2 = 64;
-
-$edad_oficial = 55;
-
-if ($edad_oficial >= $edad1 && $edad_oficial <= $edad2) {
-    echo "Esta en edad de trabajar";
-} else {
-    echo "No, no esta en edad de trabajar";
-}
-
-echo "<hr/>";
-// Ejemplo numero 2. Usando el or (o) 
-
-$pais = "Mexico";
-
-if ($pais == "Mexico" || $pais == "España" || $pais == "Colombia") {
-    echo "En este pais se habla español";
-} else {
-    echo "En esta pais no se habla español";
+switch ($dia) {
+    case 1:
+        echo "Lunes";
+        break;
+    case 2:
+        echo "Martes";
+        break;
+    case 3:
+        echo "Miercoles";
+        break;
+    case 4:
+        echo "Jueves";
+        break;
+    case 5:
+        echo "Viernes";
+        break;
+    case 6:
+        echo "Es fin de semana";
+        break;
 }
