43b5ef63dd77e48e2683fdc3e057f9ce4597a6a0
fullstack82 <fullstack8210@gmail.com>
2021-02-12 22:58:46 +0100

    of-else

---
+++ php7/condicionales/if-else.php
@@ -0,0 +1,12 @@
+<?php
+$test = 22;
+
+if ($test > 40) {
+    echo "si, $test es mayor de edad";
+} else if ($test > 35) {
+    echo "si, $edad es mayor que 35";
+} else if ($test > 30) {
+    "si, $test es mayor que 30";
+} else {
+    echo "no, $test es menor que 40, 35 y 30";
+};
