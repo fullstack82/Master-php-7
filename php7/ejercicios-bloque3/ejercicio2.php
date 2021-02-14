df5a98a4653d27f929b237eb4ee957ffcd1363a5
fullstack82 <fullstack8210@gmail.com>
2021-02-14 16:08:04 +0100

    verificacion de emails ejercicio numero 3

---
+++ php7/ejercicios-bloque3/ejercicio2.php
@@ -0,0 +1,27 @@
+<?php
+/*
+-  Ejercicio nº 2 :
+1.) Una función
+2.) Validar un email con filter_var
+3.) Recoger una variable por get y validarla
+4.) Mostrar el resultado
+*/
+
+
+function validarEmail($email)
+{
+    $status = "NO valido.";
+    if (!empty($email) && filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $status = "Valido";
+    }
+
+    return $status;
+};
+
+
+
+if(isset($_GET['email'])){
+    echo validarEmail($_GET['email']);
+}else{
+    echo "Pasa por get un email";
+}
