48775f1e16e17ecf8a1f69f6fd5bc333536cbb1f
fullstack82 <fullstack8210@gmail.com>
2021-02-16 22:55:34 +0100

    agregando datos a la base de datos

---
+++ mysql/consultas-agrupamiento/insert.sql
@@ -0,0 +1,11 @@
+/*Inserts para categorias*/
+INSERT INTO categorias VALUES(null, 'COD', 'Accion');
+INSERT INTO categorias VALUES(null,'FIFA', 'Deportes');
+
+INSERT INTO categorias VALUES(null,'DISNEY', 'Entretenimiento');
+
+INSERT INTO categorias VALUES(null,'DORA', 'Infantiles');
+
+
+
+/*Inserts para entradas*/
