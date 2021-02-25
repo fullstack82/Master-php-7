876428f33c5b78f7764aaceac0e970f8f99f4f34
fullstack82 <fullstack8210@gmail.com>
2021-02-25 13:16:40 +0100

    registro

--- proyectoBlog-php/registro.php
+++ proyectoBlog-php/registro.php
@@ -1,9 +1,45 @@
 <?php
 // Recoger los valores del formulario de registro
-if(isset($_POST)){
+if (isset($_POST)) {
     // Recoger los valores del formulario de registro
     $nombre = isset($_POST['nombre']) ? $_POST['nombre'] : false;
     $apellidos = isset($_POST['apellidos']) ? $_POST['apellidos'] : false;
     $email = isset($_POST['email']) ? $_POST['email'] : false;
     $password = isset($_POST['password']) ? $_POST['password'] : false;
+
+
+    // Array de errores
+    $errores = array();
+
+
+    if (!empty($nombre) && !is_numeric($nombre) && !preg_match("/[0-9]/", $nombre)) {
+        $nombre_validado = true;
+    } else {
+        $nombre_validado = false;
+        $errores['nombre'] = "El nombre no es valido";
+    }
+
+
+    if (!empty($apellidos) && !is_numeric($apellidos) && !preg_match("/[0-9]/", $apellidos)) {
+        $apellidos_validado = true;
+    } else {
+        $apellidos_validado = false;
+        $errores['apellidos'] = "El nombre no es valido";
+    }
+
+    if (!empty($email) && filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $email_validado = true;
+    } else {
+        $email_validado = false;
+        $errores['email'] = "El email no es valido";
+    }
+
+    if (!empty($password)) {
+        $password_validado = true;
+    } else {
+        $password_validado = false;
+        $errores['email'] = "El password esta vacio";
+    }
+
+    var_dump($errores);
 }
