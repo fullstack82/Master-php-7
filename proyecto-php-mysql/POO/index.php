8c8419707ac984c7d88f02df3d1f92d8b92d766a
fullstack82 <fullstack8210@gmail.com>
2021-03-13 23:48:20 +0100

    objetos en php

--- proyecto-php-mysql/POO/index.php
+++ proyecto-php-mysql/POO/index.php
@@ -1,12 +1,31 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>php orientacion a objetos</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+// Programacion Orientada a objetos en PHP (POO)
+
+//Definir una clase (molde para crear más objetos de tipo coche con caracteristicas parecidas);
+
+// Las clases se declarar con mayuscula y con la palabra reservada "class";
+
+class Coche {
+
+    // Atributos o propiedades (variables);
+
+    public $color = "rosa";
+    public $marca = "Alpha";
+    public $modelo = "sport";
+    public $velocidad = 400;
+    public $caballos = 250;
+    public $plazas = 5;
+
+
+    // Métodos, son acciones que hace el objeto (antes funciones);
+
+    public function getColor(){
+        //($this) Significa = Busca en esta clase la propiedad X
+        return $this->color;
+    }
+
+    public function setColor(){
+        
+    }
+
+}
