f6f7f6e467c02092354da6af18bf57d7a9082790
fullstack82 <fullstack8210@gmail.com>
2021-03-14 18:21:37 +0100

    orientacion a objetos 1º parte

--- poo/orientacion-objetos/index.php
+++ poo/orientacion-objetos/index.php
@@ -5,7 +5,8 @@
 
 // Las clases se definen con la palabra reservada "class" y siempre empiezan por mayusculas
 
-class Coche{
+class Coche
+{
     public $color = "lila";
     public $marca = "alpha";
     public $modelo = "sport";
@@ -15,25 +16,46 @@ class Coche{
 
 
     // Metodos, son acciones que hace el objeto (antes se conocian como funciones);
-    public function getColor(){
+    public function getColor()
+    {
         // Busca en esta clase la propiedad X
         return $this->color;
     }
-    public function setColor($color){
+    // Cambiar valor a la propiedad en este caso color
+    public function setColor($color)
+    {
         $this->color = $color;
     }
 
-    public function acelerar(){
+    public function acelerar()
+    {
         $this->velocidad++;
     }
 
-    public function frenar(){
+    public function frenar()
+    {
         $this->velicidad--;
     }
     // Devolvemos el valor que tiene el método
-    public function getVelocidad(){
+    public function getVelocidad()
+    {
         return $this->velocidad;
     }
+}    // Fin definicion de la clase
 
-}
+// Crear un objeto o instanciar la clase
+    $coche = new Coche();
+
+    var_dump($coche);
+echo "<br/>";
+
+// Usar los metodos;
+echo $coche->getVelocidad();
+echo "<br/>";
+echo $coche->getColor();
+echo "<br/>";
+$coche->setColor("amarillo");
+echo "El color del coche es: ".$coche->getColor();
+echo "<br/>";
+
 
